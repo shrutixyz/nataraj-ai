60585ef036f64c37e8af258412d09b1cce4b77cc
aakzsh <aakashferrari@gmail.com>
2024-07-10 21:00:18 +0530

    mobile app: setup constants, screens, routes

---
+++ app_flutter/lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const text = Colors.white;
+  static const background = Color.fromRGBO(24, 25, 26, 1);
+  static const secondaryBg = Color.fromRGBO(217, 217, 217, 0.1);
+  static const pastelRed = Color.fromRGBO(255, 112, 112, 1);
+  static const pastelGreen = Color.fromRGBO(152, 255, 126, 1);
+  static const black = Colors.black;
+  static const green = Color.fromRGBO(84, 255, 91, 1);
+  static const red = Color.fromRGBO(255, 84, 84, 1);
+  static const yellow1 = Color.fromRGBO(255, 238, 149, 1);
+  static const yellow2 = Color.fromRGBO(227, 184, 73, 1);
+}
---
+++ app_flutter/lib/utils/constants.dart
@@ -0,0 +1,11 @@
+class RoutesString{
+  static const loginPageRoute = '/login';
+  static const welcomePageRoute = '/welcome';
+  static const notFoundRoute = '/404';
+  static const homePageRoute = '/home';
+  static const configurationPageRoute = '/config';
+  static const dancePageRoute = '/dance';
+  static const generatingReportPageRoute = '/generatereport';
+  static const allReportsPageRoute = '/allreports';
+  static const singleReportPageRoute = '/singleReportPageRoute';
+}
