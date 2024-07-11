fa41fb806fc1b43fef958871738b06a288624fce
aakzsh <aakashferrari@gmail.com>
2024-07-11 20:37:12 +0530

    add firebase app, setup provider

---
+++ app_flutter/lib/models/dummy.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// using provider for state management
+class CounterModel with ChangeNotifier {
+  int _counter = 0;
+
+  int get counter => _counter;
+
+  void increment() {
+    _counter++;
+    notifyListeners();
+  }
+}
