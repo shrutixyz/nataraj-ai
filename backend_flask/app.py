6ef53e523ad78abbc8802550201b70d55b3660ad
aakzsh <aakashferrari@gmail.com>
2024-07-18 21:11:08 +0530

    add: firebase create data method

--- backend_flask/app.py
+++ backend_flask/app.py
@@ -1,18 +1,37 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from read_files import read_md_files
+from firebase_functions import push_data, check_document_exists, check_user_exists
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    """Returns a JSON response containing the content of all .md files in the static/blogs directory."""
-    return "Hi there, you've stumbled across the backend API of Nataraj AI"
+    return jsonify("Hi there, you've stumbled across the backend API of Nataraj AI")
+
 
 @app.route('/blogs')
 def get_blogs():
   """Returns a JSON response containing the content of all .md files in the static/blogs directory."""
   blog_data = read_md_files('static/blogs')
   return jsonify(blog_data)
+
+
+@app.route('/createuserdata/<uid>')
+def create_user_data(uid):
+  response_model = {"success": False, "message": "NA"}
+  doc_exists = check_document_exists("users", uid)
+  if not doc_exists:
+     user = check_user_exists(uid)
+     response_model["success"] = True
+     if user!=None:
+        push_data(user)
+        response_model["message"] = "Data Added Successfully!"
+     else:
+        response_model["message"] = "User with such UID doesn't exist"
+  else:
+     response_model["message"] = "Data already exists"
+  return jsonify(response_model)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ backend_flask/firebase_functions.py
@@ -0,0 +1,48 @@
+import firebase_admin
+from firebase_admin import credentials, firestore, auth
+
+# Path to the service account key file
+cred = credentials.Certificate('nataraj-admin-key.json')
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred)
+
+# Initialize Firestore
+db = firestore.client()
+
+datamodel = {
+
+}
+
+def push_data(user):
+    data = {
+        "name": user.display_name,
+        "email": user.email,
+        "avatars": [],
+        "ispremium": False,
+        "premiumexpiry": None,
+        "profilepicture":"https://i.ibb.co/nQhcZSc/pfp.png",
+        "projects": []
+    }
+    doc_ref = db.collection("users").document(user.uid).set(data)
+    print(f'Document added, {doc_ref}')
+
+
+def check_document_exists(collection_name, document_id):
+    doc_ref = db.collection(collection_name).document(document_id)
+    doc = doc_ref.get()
+    if doc.exists:
+        print(f'Document with ID {document_id} exists.')
+        return True
+    else:
+        print(f'Document with ID {document_id} does not exist.')
+        return False
+
+def check_user_exists(uid):
+    try:
+        user = auth.get_user(uid)
+        print(f'User with UID {uid} exists.')
+        return user
+    except auth.UserNotFoundError:
+        print(f'User with UID {uid} does not exist.')
+        return None
