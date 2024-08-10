10d69e5a49770f05c4b7f719959b140c747fbb31
Shruti Gupta <shrutigupta5555@gmail.com>
2024-08-10 21:23:49 +0530

    refactor project file

--- web_react/src/pages/Project/Project.tsx
+++ web_react/src/pages/Project/Project.tsx
@@ -1,4 +1,4 @@
-import Styles from "./Project.module.css"
+import Styles from "./Project.module.css";
 
 import React, { useEffect, useRef, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
@@ -10,10 +10,11 @@ import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import loader from "../../assets/loader.svg";
 import { timeStamp } from "console";
-import playIcon from '../../assets/play.svg';
-import pauseIcon from '../../assets/pause.svg';
-import resetIcon from '../../assets/reset.svg';
+import playIcon from "../../assets/play.svg";
+import pauseIcon from "../../assets/pause.svg";
+import resetIcon from "../../assets/reset.svg";
 import googleplay from "../../assets/googleplay.png";
+import quote from "../../assets/quote.svg";
 
 function Project() {
   const { unityProvider, sendMessage, isLoaded } = useUnityContext({
@@ -27,188 +28,121 @@ function Project() {
   const navigate = useNavigate();
   const isLoggedIn = useSelector((state: any) => state.auth.isLoggedIn);
   const endpoint = useSelector((state: any) => state.backend.endpoint);
-  const [nextTimeStamp, setnextTimeStamp] = useState(0.00);
-  const [timeStampList, settimeStampList] = useState([0.00]);
-  const [lyricsList, setLyricsList] = useState([""]);
-  const [currentLyrics, setcurrentLyrics] = useState("");
-  const [isAPILoading, setisAPILoading] = useState(true);
-  const [isUnityLoaded, setisUnityLoaded] = useState(false);
-
-  const [isPlaying, setisPlaying] = useState(false);
-  const [isReset, setisReset] = useState(true);
-  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
-  const [currentTime, setCurrentTime] = useState(0);
-  const [duration, setduration] = useState(0);
-  const [widthProgressBar, setwidthProgressBar] = useState(0.0);
-  const [isEnded, setIsEnded] = useState(false);
-
-  const [stepSequence, setstepSequence] = useState("");
-
-  useEffect(function () {
-    if (isLoaded && !isAPILoading)
-    {
-      console.log("unity", isLoaded);
-      sendStepSequence();
-      setisUnityLoaded(isLoaded);
-    }  
-  }, [isLoaded]);
-
-  useEffect(function() {
-    if (isEnded){
-      handleReset(false);
-      setisReset(true);
-    } 
-  }, [isEnded])
-
-  useEffect(function() {
-    console.log("currentLyrics: ", currentLyrics);
-  }, [currentLyrics])
-
-  const [project, setProject] = useState({
-      "avatar": "",
-      "choreography": "",
-      "danceform": "",
-      "duration": 0,
-      "loading": 0,
-      "owner": "",
-      "projectID": "",
-      "projectName": "",
-      "song": "",
-      "state": 0,
-      "title": "",
-      "visibility": ""
-  });
   const { projectId } = useParams();
 
-  
-  const handleTimeUpdate =  () => {
-    console.log("onreset", currentTime, nextTimeStamp)
-    if ((currentTime> nextTimeStamp && currentTime != 0))
-    {
-      console.log("time:", currentTime, nextTimeStamp, currentTime> nextTimeStamp)
+  const [project, setProject] = useState<any>(null);
+  const [nextTimeStamp, setNextTimeStamp] = useState(0.0);
+  const [timeStampList, setTimeStampList] = useState<number[]>([0.0]);
+  const [lyricsList, setLyricsList] = useState<string[]>([""]);
+  const [currentLyrics, setCurrentLyrics] = useState("");
+  const [isAPILoading, setIsAPILoading] = useState(true);
+  const [isUnityLoaded, setIsUnityLoaded] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isReset, setIsReset] = useState(true);
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [widthProgressBar, setWidthProgressBar] = useState(0.0);
+  const [isEnded, setIsEnded] = useState(false);
+  const [stepSequence, setstepSequence] = useState("1_2_3_5");
+
+
+  useEffect(() => {
+    fetchProject();
+  }, []);
+
+  useEffect(() => {
+    if (isLoaded && !isAPILoading) {
+      console.log("unity", stepSequence);
+      sendStepSequence();
+      setIsUnityLoaded(isLoaded);
+    }
+
+    if (isEnded) {
+      handleReset(false);
+      setIsReset(true);
+    }
+
+    if (currentTime === 0) {
+      setCurrentLyrics(lyricsList[0]);
+      setNextTimeStamp(timeStampList[1]);
+    } else {
+      handleTimeUpdate();
+    }
+
+    console.log("loading", isAPILoading, !isLoaded, isAPILoading || !isLoaded);
+  }, [isLoaded, isAPILoading, isEnded, currentTime]);
+
+  useEffect(() => {
+    if (project) {
+      setIsAPILoading(false);
+      const tempAudio = new Audio(project.song);
+      setAudio(tempAudio);
+
+      const updateCurrentTime = () => setCurrentTime(tempAudio.currentTime);
+      const updateDuration = () => {
+        setDuration(tempAudio.duration);
+        extractLyricsJSON(tempAudio.duration);
+      };
+
+      tempAudio.addEventListener("timeupdate", updateCurrentTime);
+      tempAudio.addEventListener("loadedmetadata", updateDuration);
+      tempAudio.addEventListener("ended", () => setIsEnded(true));
+
+      return () => {
+        tempAudio.removeEventListener("timeupdate", updateCurrentTime);
+        tempAudio.removeEventListener("loadedmetadata", updateDuration);
+        tempAudio.pause();
+        setCurrentTime(0);
+      };
+    }
+  }, [project]);
+
+  function handleTimeUpdate() {
+    if (currentTime > nextTimeStamp && currentTime !== 0) {
       const idx = timeStampList.indexOf(nextTimeStamp);
-      if (idx + 1 < timeStampList.length)
-      {
-        setnextTimeStamp(timeStampList[idx+1])
+      if (idx + 1 < timeStampList.length) {
+        setNextTimeStamp(timeStampList[idx + 1]);
       }
-      setcurrentLyrics(lyricsList[idx]);
-    } 
-    // update progress bar
-    const width = currentTime / duration;
-    
-    console.log("width: ",currentTime, duration, currentTime/duration)
-    if (duration != 0)
-    {
-      setwidthProgressBar(width * 90);
+      setCurrentLyrics(lyricsList[idx]);
+    }
+
+    if (duration !== 0) {
+      setWidthProgressBar((currentTime / duration) * 90);
     }
   }
 
-  const resetNextTimestamp  = (currentTimestamp: number) => {
-    const tempTimestamp = findNextGreatest(timeStampList, currentTimestamp);
-    if (tempTimestamp != -1)
-    {
-      setnextTimeStamp(tempTimestamp)
-    }
-  }
-
-  const handleReset = (startPlaying: boolean) => {
-    console.log("inside reset");
-    // reset lyrics sync
-    console.log("reset lyrics", timeStampList[1], lyricsList[0]);
-    setnextTimeStamp(timeStampList[1]);
-    setcurrentLyrics(lyricsList[0]);
+  function handleReset(startPlaying: boolean) {
+    setNextTimeStamp(timeStampList[1]);
+    setCurrentLyrics(lyricsList[0]);
     setIsEnded(false);
-    if (audio)
-    {
-      if (startPlaying)
-      {
-        const playState = 2;
-        setisPlaying(true);
-        audio.currentTime = 0;
-        audio.play()
-        sendMessage("Michelle@Idle", "ControlPlayState", playState);
-      }
-      else
-      {
-        const playState = 3;
-        setisPlaying(false);
-        audio.currentTime = 0;
-        audio.pause();
-        sendMessage("Michelle@Idle", "ControlPlayState", playState);
-      }
 
-    }  
-
-  }
-
-  function findNextGreatest(arr: number[] | any[], num: number) {
-    // Find the index of the given number
-    const index = arr.indexOf(num);
-  
-    // If the number is not found or it's the last element, return -1
-    if (index === -1 || index === arr.length - 1) {
-      return -1;
-    }
-  
-    // Iterate through the array from the next index to find the first greater number
-    for (let i = index + 1; i < arr.length; i++) {
-      if (arr[i] > num) {
-        return arr[i];
-      }
-    }
-  
-    // If no greater number is found, return -1
-    return -1;
-  }
-
-
-
-  const extractLyricsJSON = (projectt: any) => {
-    const firstIndex = projectt.choreography.indexOf("{")
-    const lastIndex = projectt.choreography.lastIndexOf("}")
-    const jsonString = projectt.choreography.substring(firstIndex, lastIndex+1);
-    if (jsonString)
-    {
-      const lyricsJSON = JSON.parse(projectt.choreography.substring(firstIndex, lastIndex+1));
-      const timeStamp: Float32List = Object.keys(lyricsJSON).map(e => parseFloat(e));
-  
-      // populate lyrics list
-      const lyrics: string[] = Object.keys(lyricsJSON).map(e => lyricsJSON[e].lyrics);
-      const stepsList: Float32List = Object.keys(lyricsJSON).map(e => lyricsJSON[e].steps);
-
-      
-      settimeStampList(timeStamp);
-      setLyricsList(lyrics);
-      setnextTimeStamp(timeStamp[1]);
-      setcurrentLyrics(lyrics[0]);
-      console.log("chemck ", stepsList)
-      setstepSequence(stepsList.join("_"));
-      setisAPILoading(false);
+    if (audio) {
+      const playState = startPlaying ? 2 : 3;
+      setIsPlaying(startPlaying);
+      audio.currentTime = 0;
+      startPlaying ? audio.play() : audio.pause();
+      sendMessage("Michelle@Idle", "ControlPlayState", playState);
     }
   }
 
-  const fetchProject = async () => {
+  async function fetchProject() {
     try {
       const res = await axios.get(`${endpoint}/fetchproject/${projectId}`);
-    //   const proj = {
-    //     "avatar": "",
-    //     "choreography": "```json\n{\n  \"0.00\": {\n    \"steps\": \"18\",\n    \"lyrics\": \"Cuz the players gonna play play play play play and the haters gonna hate hate hate hate hate baby\"\n  },\n  \"2.25\": {\n    \"steps\": \"1\",\n    \"lyrics\": \"I'm just gonna shake shake shake shake shake Shake it off shake it off\"\n  },\n  \"5.10\": {\n    \"steps\": \"16\",\n    \"lyrics\": \"Heartbreak is gonna break break break break break and my big tears gonna fake fake fake fake fake baby I'm just gonna shake shake shake shake shake shake it off shake it off\"\n  },\n  \"7.00\": {\n    \"steps\": \"14\",\n    \"lyrics\": \"I never miss a beat\"\n  }\n}\n```",
-    //     "danceform": "Freestyle, ",
-    //     "duration": 25.842999999999996,
-    //     "loading": 0,
-    //     "owner": "CPJ1kLB2Bud9CQWMcepUVnH84xE2",
-    //     "projectID": "ksvf4un",
-    //     "projectName": "astonishing warrior",
-    //     "song": "https://storage.googleapis.com/nataraj-ai.appspot.com/uploads/modified-ksvf4un.mp3",
-    //     "state": 2,
-    //     "title": "Taylor Swift - Shake It Off (Taylor's Version) (Lyric Video).mp3",
-    //     "visibility": "private"
-    //   }
-    // setProject(proj)
-    extractLyricsJSON(res.data["project"]); 
-    // hahaha
-    setProject(res.data["project"])
-      
+      setProject(res.data["project"]);
+
+      if (res.data["project"]["visibility"] === "private") {
+        const valid = res.data["project"]["owner"] === uid;
+        if (!valid) {
+          Swal.fire({
+            title: "Error!",
+            text: "You don't have access to this page!",
+            confirmButtonColor: "#18191A",
+            confirmButtonText: "okay",
+          });
+          navigate("/dashboard");
+        }
+      }
     } catch (error) {
       Swal.fire({
         title: "Error!",
@@ -217,130 +151,157 @@ function Project() {
         confirmButtonText: "okay",
       });
     }
+  }
+
+  function extractLyricsJSON(durationtemp: number) {
+    try {
+      const firstIndex = project.choreography.indexOf("{");
+      const lastIndex = project.choreography.lastIndexOf("}");
+      const jsonString = project.choreography.substring(firstIndex, lastIndex + 1);
+      const lyricsJSON = JSON.parse(jsonString);
+      
+      const newTimeStamp = spreadLyrics(
+        Object.values(lyricsJSON).map((item: any) => item.lyrics),
+        durationtemp
+      );
+      
+      setTimeStampList(newTimeStamp);
+      setLyricsList(Object.values(lyricsJSON).map((item: any) => item.lyrics));
+      setNextTimeStamp(newTimeStamp[1]);
+      setCurrentLyrics((Object.values(lyricsJSON)[0] as any).lyrics);
+      setstepSequence((Object.values(lyricsJSON) as any).map((item: any) => item.steps).join("_"));
+    } catch (error) {
+      Swal.fire({
+        title: "Error!",
+        text: "Unable to fetch and parse lyrics or steps.",
+        confirmButtonColor: "#18191A",
+        confirmButtonText: "okay",
+      });
+    }
+  }
+
+  const spreadLyrics = (lyrics: string[], total_time: number): number[] => {
+    const timestamps: number[] = [];
+    const totalLength = lyrics.reduce((sum, lyric) => sum + lyric.length, 0);
+
+    let currentTime = 0;
+    for (let i = 0; i < lyrics.length; i++) {
+      timestamps.push(currentTime);
+      currentTime += (lyrics[i].length / totalLength) * total_time;
+    }
+
+    return timestamps;
   };
 
-  useEffect(() => {
-    if (project) {
-      
-      setisAPILoading(false);
-      let tempAudio = new Audio(project.song);
-      setAudio(tempAudio);
+  function handlePlayPause() {
+    const playState = !isReset ? 2 : isPlaying ? 0 : 1;
+    setIsPlaying(!isPlaying);
 
-      const updateCurrentTime = () => {
-        setCurrentTime(tempAudio.currentTime);
-      };
-
-      const updateDuration = () => {
-        setduration(tempAudio.duration);
-      };
-
-      const handleEnded = () => {
-        setIsEnded(true);
-      };
-
-      // attach event listener
-      tempAudio.addEventListener('timeupdate', updateCurrentTime);
-      tempAudio.addEventListener('loadedmetadata', updateDuration);
-      tempAudio.addEventListener('ended', handleEnded);
-
-      // Cleanup event listener and pause audio when component unmounts or URL changes
-      return () => {
-        tempAudio.removeEventListener('timeupdate', updateCurrentTime);
-        tempAudio.removeEventListener('loadedmetadata', updateCurrentTime);
-
-        tempAudio.pause();
-        setCurrentTime(0); // Reset the time
-    };
-    }
-  }, [project]);
-
-  useEffect(() => {
-    fetchProject();
-  }, []);
-
-  useEffect(() => {
-    console.log("loading", isAPILoading, !isLoaded, isAPILoading || !isLoaded);
-  }, [isAPILoading, isLoaded]);
-
-  useEffect(() => {
-    handleTimeUpdate();
-    if (currentTime == 0)
-    {
-      setcurrentLyrics(lyricsList[0]);
-      setnextTimeStamp(timeStampList[1]);
-    }
-  }, [currentTime]);
-
-  function handlePlayPause()
-  {
-    let playState;
-    if (!isReset)
-    {
-      playState = 2;
-      setisReset(!isReset);
-      setisPlaying(true);
+    if (playState) {
       audio?.play();
-    } 
-    else
-    {
-      playState = isPlaying ? 0 : 1
-      setisPlaying(!isPlaying);
-      if(playState)
-      {
-        audio?.play();
-      } else {
-        audio?.pause();
-      }
-    } 
+    } else {
+      audio?.pause();
+    }
     sendMessage("Michelle@Idle", "ControlPlayState", playState);
   }
 
-  function sendStepSequence()
-  {
-    console.log("hereee ", stepSequence)
+  function sendStepSequence() {
     sendMessage("Michelle@Idle", "SetStepSequence", stepSequence);
   }
 
-  return <>
-    <Nav></Nav>
-    {isAPILoading? <div className={Styles.loading}>
-        <img src={loader} className={Styles.loader} alt="" />
-        <p>loading your projects</p>
-        </div> :
-    <div>
-    <p className={Styles.subtitle}>{project?.projectName}</p>
-    <div className={Styles.container}>
-        <Unity unityProvider={unityProvider} style={{
-            width: '50vw',
-            height: '40vh',
-            }}
-        />
-    </div>
-    <br />
-    <div className={Styles.bottomContainer}>
-      <div className={Styles.ProgressBarContainer}>
-        <div className={Styles.ProgressBar}></div>
-        <div className={Styles.ProgressBarInner}  style={{width: `${widthProgressBar}%`}}></div>
-        <br />
-        <div className={Styles.buttonsContainer}>
-          {isPlaying ?
-            <button className={Styles.playPauseBtn} disabled={!isUnityLoaded}>
-              <img  className="pauseBtn" onClick={handlePlayPause} src={pauseIcon} alt="" /> :
-            </button>:
-            <button className={Styles.playPauseBtn} disabled={!isUnityLoaded}>
-              <img  className="playBtn" onClick={handlePlayPause} src={playIcon} alt="" /> :
-            </button>}
-            <button disabled={!isUnityLoaded} className={Styles.playPauseBtn}>
-              <img src={resetIcon} onClick={() => handleReset(true)} alt="" />
-            </button>
+  return (
+    <>
+      <Nav></Nav>
+      {isAPILoading ? (
+        <div className={Styles.loading}>
+          <img src={loader} className={Styles.loader} alt="" />
+          <p>Fetching your Nataraj Choreographed Project</p>
         </div>
-      </div>
-      <p>{currentLyrics}</p>
-    </div>
-            {/* className={Styles.googleplayimg} /> */}
+      ) : (
+        <div>
+          <p className={Styles.subtitle}>Project: {project?.projectName}</p>
+          <div className={Styles.container}>
+            <Unity
+              unityProvider={unityProvider}
+              style={{
+                width: "80vw",
+                height: "60vh",
+              }}
+            />
+          </div>
+          <br />
+          <div className={Styles.bottomContainer}>
+            <div className={Styles.ProgressBarContainer}>
+              <div className={Styles.ProgressBar}></div>
+              <div
+                className={Styles.ProgressBarInner}
+                style={{ width: `${widthProgressBar}%` }}
+              ></div>
+              <br />
+              <div className={Styles.buttonsContainer}>
+                {isPlaying ? (
+                  <button
+                    className={Styles.playPauseBtn}
+                    disabled={!isUnityLoaded}
+                  >
+                    <img
+                      className="pauseBtn"
+                      onClick={handlePlayPause}
+                      src={pauseIcon}
+                      alt=""
+                    />{" "}
+                    :
+                  </button>
+                ) : (
+                  <button
+                    className={Styles.playPauseBtn}
+                    disabled={!isUnityLoaded}
+                  >
+                    <img
+                      className="playBtn"
+                      onClick={handlePlayPause}
+                      src={playIcon}
+                      alt=""
+                    />{" "}
+                    :
+                  </button>
+                )}
+                <button
+                  disabled={!isUnityLoaded}
+                  className={Styles.playPauseBtn}
+                >
+                  <img
+                    src={resetIcon}
+                    onClick={() => handleReset(true)}
+                    alt=""
+                  />
+                </button>
+              </div>
+            </div>
+            <div className={Styles.lyricbox}>
+              <img src={quote} className={Styles.quote} alt="" />
+              <p className={Styles.currentlyrics}>{currentLyrics}</p>
+            </div>
+          </div>
 
-    </div>}
-  </>
+          <div className={Styles.playStoreContainer}>
+            <center>
+              <p>Practice it on Android App</p>
+            </center>
+            <img
+              title="google play"
+              src={googleplay}
+              onClick={() =>
+                (window.location.href =
+                  "https://play.google.com/store/apps/details?id=co.zanie.nataraj")
+              }
+              className={Styles.googleplayimg}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Project;
